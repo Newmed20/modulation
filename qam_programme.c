a1c1f46f3dc6c562b7584e1373af4c66c3f87b66
Mohammed Jadid <hamoda.style.000@gmail.com>
2025-01-02 09:41:59 +0100

    last version

--- qam_programme.c
+++ qam_programme.c
@@ -6,7 +6,7 @@
 /*   By: mjadid <mjadid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 09:23:25 by mjadid            #+#    #+#             */
-/*   Updated: 2025/01/02 09:23:26 by mjadid           ###   ########.fr       */
+/*   Updated: 2025/01/02 09:38:03 by mjadid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,11 @@ int main()
         printf("\nVeuillez entrer le niveau M  : ");
         scanf("%d", &M);
 
-        printf("Veuillez entrer le bruit de phase Bφ en radians : ");
+        printf("Veuillez entrer le bruit de phase Bϕ : ");
         double Bphi;
         scanf("%lf", &Bphi);
 
-        printf("Veuillez entrer le bruit de l'energie Be en radians : ");
+        printf("Veuillez entrer le bruit de l'energie Be  : ");
         double Be;
         scanf("%lf", &Be);
 
@@ -64,7 +64,7 @@ int main()
             fprintf(file, "(%d colonnes ; %d lignes) - %d (nombre de symboles exclus)\n", X, X, Y);
         }
 
-        fprintf(file, "\n%-8s|%-10s|%-10s|%-15s|%-14s|%-27s|%-14s\n", "CODE D", "CODE B", "coord", "Énergie", "Phase" , "Phase_Bruitée" , "Énergie_Bruitée");
+        fprintf(file, "\n%-8s|%-10s|%-10s|%-15s|%-14s|%-27s|%-14s\n", "CODE D", "CODE B", "coord", "Énergie", "Phase", "Phase_Bruitée", "Énergie_Bruitée");
 
         int k = (X / 2) - 1;
         int max = 2 * k + 1;
@@ -75,12 +75,15 @@ int main()
         {
             for (int j = max; j >= min; j -= 2)
             {
+                if (code >= M)
+                    break; // Quitte la boucle interne si on a généré M valeurs
+
                 // Calculer l'énergie
                 double E = pow(i, 2) + pow(j, 2);
 
-                 // Calculer la phase (en radians)
+                // Calculer la phase (en radians)
                 double phase = atan2(j, i);
-                
+
                 // Convertir la phase en multiples de pi
                 char phase_str[20];
                 if (phase > 0)
@@ -88,18 +91,19 @@ int main()
                 else
                     phase = (phase / M_PI) + 2;
                 sprintf(phase_str, "%.2f*pi", phase);
-                
+
                 // Calcul de la phase bruitée
                 char phase_noise[40];
                 double phase_min = phase - Bphi;
                 double phase_max = phase + Bphi;
                 sprintf(phase_noise, "[%.2f*pi; %.2f*pi]", phase_min, phase_max);
-                
+
                 // Calcul de l'énergie bruitée
                 double en_min = E - Be;
                 double en_max = E + Be;
                 char energie_noise[40];
                 sprintf(energie_noise, "[%.2f; %.2f]", en_min, en_max);
+
                 // Conversion du code en binaire
                 char binary[33];
                 for (int b = m - 1; b >= 0; b--)
@@ -109,9 +113,11 @@ int main()
                 binary[m] = '\0';
 
                 // Écriture des résultats avec alignement
-                fprintf(file, "%-8d|%-10s|(%3d,%3d) |E = %-10.2f|φ = %-10s|φ ∈%-23s|E ∈%-10s\n", code, binary, i, j, E, phase_str,phase_noise, energie_noise);
+                fprintf(file, "%-8d|%-10s|(%3d,%3d) |E = %-10.2f|φ = %-10s|φ ∈%-23s|E ∈%-10s\n", code, binary, i, j, E, phase_str, phase_noise, energie_noise);
                 code++;
             }
+            if (code >= M)
+                break; // Quitte la boucle externe si on a généré M valeurs
         }
 
         fclose(file);
